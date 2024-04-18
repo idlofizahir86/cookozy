0b8993e7869d6aee951b3d826ea8e111beca4aad
idlofizahir86 <idlofizahir86@gmail.com>
2024-04-18 23:53:06 +0700

    cara lain get

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -63,14 +63,6 @@ class RecipeController extends Controller
     {
         $baseUrl = '';
 
-        $idRecipe = $id;
-
-        if (!empty($idRecipe)){
-            $idRecipe = $id;
-        } else {
-            $idRecipe = "";
-        }
-
         if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1') {
             // Lokal (Development)
             $baseUrl = 'http://localhost:8000';
@@ -79,34 +71,28 @@ class RecipeController extends Controller
             $baseUrl = 'https://cookozy-pwohh4kjqa-et.a.run.app'; // Ganti dengan URL produksi Anda
         }
 
-        $uri = "{$baseUrl}/api/recipes/{$idRecipe}";
+        $url = "{$baseUrl}/api/recipes/{$id}";
+        dd($url);
 
-        if ($uri !== null) {
-            // Lakukan permintaan ke API untuk mendapatkan data resep berdasarkan ID
-            $request = Request::create($uri, 'GET');
-            $response = app()->handle($request);
+        // Lakukan permintaan ke API untuk mendapatkan data resep berdasarkan ID
+        $response = Http::get($url);
 
-            // Memeriksa apakah permintaan berhasil
-            if ($response->getStatusCode() === 200) {
-                // Ubah respons ke JSON
-                $jsonContent = $response->getContent();
+        // Memeriksa apakah permintaan berhasil
+        if ($response->successful()) {
+            // Ubah respons ke dalam array
+            $data = $response->json();
 
-                // Decode JSON ke dalam array
-                $data = json_decode($jsonContent, true);
+            // Ambil data resep dari respons JSON
+            $recipe = $data['data'];
 
-                // Ambil data resep dari response JSON
-                $recipe = $data['data'];
-                // Tampilkan view dengan data resep
-                return view('detailRecipe', compact('recipe'));
-            } else {
-                // Jika permintaan gagal, tampilkan halaman error
-                abort(404);
-            }
+            // Tampilkan view dengan data resep
+            return view('detailRecipe', compact('recipe'));
         } else {
-            // Penanganan jika $uri null
+            // Jika permintaan gagal, tampilkan halaman error
             abort(404);
         }
     }
+
 
 
 
